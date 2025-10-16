173296fb0c418483cc8177d2913803b66cee3835
Eduardo Manuel Padilla Niño <epadillan2300@alumno.ipn.mx>
2025-10-16 10:59:17 -0600

    Add PHOTO_LIST constant for image paths
    Defined a constant array of photo file paths.

---
+++ js/foto-list.js
@@ -0,0 +1,12 @@
+// Aqui solo definimos la lista de fotos
+const PHOTO_LIST = [
+  'images/conejo.jpg',
+  'images/lobo.jpg',
+  'images/gato.jpg',
+  'images/mono.jpg',
+  'images/panda.jpg',
+  'images/perro.jpeg',
+  'images/suricata.jpg',
+  'images/tigre.jpg',
+  'images/zorro.jpg'
+];
